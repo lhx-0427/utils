5699cf25e8ecc79ccba11c1bca59c3d991c786d8
lhx <2122537936@qq.com>
2024-04-30 09:51:26 +0800

    新增子模块

---
+++ demo1/src/main/java/com/example/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * @Author：lihouxiang
+ * @Package：com.example
+ * @Date：${DATE} ${TIME}
+ * @Classname：${NAME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
